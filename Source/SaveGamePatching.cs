24c495464f7b831ddfe0c1b9f9068b784f6a1f67
Chris Bornholdt <chris.bornholdt@gmail.com>
2018-06-12 12:27:21 -0400

    Added NotPresentPatches

--- Source/SaveGamePatching.cs
+++ Source/SaveGamePatching.cs
@@ -11,6 +11,7 @@ namespace RimWorld
 	public static class SaveGamePatches
 	{
 		public static List<PatchOperation> patches;
+		public static List<PatchOperation> notPresentPatches;
 	}
 
 	[StaticConstructorOnStartup]
@@ -19,6 +20,7 @@ namespace RimWorld
 		static HarmonyPatches ()
 		{
 			SaveGamePatches.patches = new List<PatchOperation> ();
+			SaveGamePatches.notPresentPatches = new List<PatchOperation>();
 			var harmony = HarmonyInstance.Create("saveGamePatching");
 			harmony.PatchAll(Assembly.GetExecutingAssembly());
 		}
@@ -42,7 +44,7 @@ namespace RimWorld
 			ScribeMetaHeaderUtility.LoadGameDataHeader (ScribeMetaHeaderUtility.ScribeHeaderMode.Map, true);
 			//BEGIN PATCH
 			DeepProfiler.Start ("Patching Save Game");
-			ApplySGPatches ();
+			ApplySaveGamePatches ();
 			DeepProfiler.End ();
 			//END PATCH
 			if (Scribe.EnterNode ("game")) {
@@ -58,23 +60,38 @@ namespace RimWorld
 			return false;  //Replace actual LoadGameFromSaveFile method
 		}
 
-		private static void ApplySGPatches()
+		private static void ApplySaveGamePatches()
 		{
-			LoadGameFromSaveFilePatch.LoadSGPatches ();
+			LoadGameFromSaveFilePatch.LoadSaveGamePatches ();
+            LoadGameFromSaveFilePatch.LoadNotPresentPatches ();
 			Log.Message (string.Format ("Applying {0:d} SaveGamePatches", SaveGamePatches.patches.Count));
 			foreach (PatchOperation patch in SaveGamePatches.patches) 
 				patch.Apply (Scribe.loader.curXmlParent.OwnerDocument);
+			Log.Message(string.Format("Applying {0:d} NotPresent Patches", SaveGamePatches.notPresentPatches.Count));  
+            foreach (PatchOperation patch in SaveGamePatches.notPresentPatches) 
+                patch.Apply (Scribe.loader.curXmlParent.OwnerDocument);
 		}
 
-		public static void LoadSGPatches()
+		public static void LoadSaveGamePatches()
 		{
 			SaveGamePatches.patches.Clear ();
 
-			foreach (ModContentPack mod in LoadedModManager.RunningModsListForReading.Where(m => !ScribeMetaHeaderUtility.loadedModIdsList.Contains(m.Identifier))) 
-				LoadSGPatchesFor (mod);
+			foreach (ModContentPack mod in LoadedModManager.RunningModsListForReading
+                .Where(m => !ScribeMetaHeaderUtility.loadedModIdsList.Contains(m.Identifier))) 
+				LoadSaveGamePatchesFor (mod);
 		}
+        
+        public static void LoadNotPresentPatches()
+        {
+            SaveGamePatches.patches.Clear ();
+            //LoadedModManager only has Ids ...
+            foreach (ModMetaData mod in ScribeMetaHeaderUtility.loadedModIdsList
+                           .Select(id => ModLister.AllInstalledMods.FirstOrDefault(mod => mod.Identifier == id))
+                           .Where(mod => mod != null && !LoadedModManager.RunningMods.Any(modContent => modContent.Name == mod.Name)))
+                LoadNotPresentPatchesFor (mod);
+        }
 
-		private static void LoadSGPatchesFor(ModContentPack mod)	//Taken from Verse.ModContentPack.LoadPatches()
+		private static void LoadSaveGamePatchesFor(ModContentPack mod)	//Taken from Verse.ModContentPack.LoadPatches()
 		{
 			List<LoadableXmlAsset> list = DirectXmlLoader.XmlAssetsInModFolder (mod, "SaveGamePatches/").ToList<LoadableXmlAsset> ();
 			for (int i = 0; i < list.Count; i++) {
@@ -97,5 +114,32 @@ namespace RimWorld
 				} 
 			}	
 		}
+
+		private static void LoadNotPresentPatchesFor(ModMetaData modMetaData)  //Adapted from Verse.ModContentPack.LoadPatches()
+		{
+			ModContentPack mod = new ModContentPack(modMetaData.RootDir, -1, "Loadable");
+			List < LoadableXmlAsset > list = DirectXmlLoader.XmlAssetsInModFolder(mod, "NotPresentPatches/").ToList<LoadableXmlAsset>();
+			for(int i = 0; i < list.Count; i++) {
+				XmlElement documentElement = list[i].xmlDoc.DocumentElement;
+				if(documentElement.Name != "Patch") {
+					Log.Error(string.Format("Unexpected document element in patch XML; got {0}, expected 'Patch'", documentElement.Name));
+				}
+				else {
+					for(int j = 0; j < documentElement.ChildNodes.Count; j++) {
+						XmlNode xmlNode = documentElement.ChildNodes[j];
+						if(xmlNode.NodeType == XmlNodeType.Element) {
+							if(xmlNode.Name != "Operation") {
+								Log.Error(string.Format("Unexpected element in patch XML; got {0}, expected 'Operation'", documentElement.ChildNodes[j].Name));
+							}
+							else {
+								PatchOperation patchOperation = DirectXmlToObject.ObjectFromXml<PatchOperation>(xmlNode, false);
+								patchOperation.sourceFile = list[i].FullFilePath;
+								SaveGamePatches.notPresentPatches.Add(patchOperation);
+							}
+						}
+					}
+				}
+			}
+        }
 	}	
 }
